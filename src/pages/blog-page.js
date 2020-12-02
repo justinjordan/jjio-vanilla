29ae58f60d354d6ec7eb2e4ec5f99e2795c9e425
Justin Jordan <justin@justinjordan.io>
2020-12-01 20:09:24 -0600

    Added custom styles

--- src/pages/blog-page.js
+++ src/pages/blog-page.js
@@ -19,7 +19,7 @@ customElements.define('blog-page', class extends HTMLElement {
     try {
       const post = await getPost(slug);
 
-      this.innerHTML = `<div class="container mx-auto max-w-7xl px-8 prose-xl">
+      this.innerHTML = `<div class="blog container mx-auto max-w-7xl px-8 prose-xl">
         ${post.content.rendered}
       </div>`;
     } catch (e) {
